7696f28e2d6831781a3dc8e626ecd2d18a43ae4c
BrandonNelson-Creator <n.ellyson@hotmail.com>
2021-09-27 13:09:59 -0300

    updated methods

--- Program.cs
+++ Program.cs
@@ -6,10 +6,42 @@ using System.Threading.Tasks;
 
 namespace Methods2
 {
+   
     class Program
     {
+        static int score;
+        static int health;
+
+        static void ShowHUD()
+        {
+            Console.WriteLine("Health = " + health);
+            Console.WriteLine("Score = " + score);
+        }
+
+        static void TakeDamage(int damage)
+        {
+            health = health - damage;
+        }
+
+        
         static void Main(string[] args)
         {
+            Console.WriteLine("Methods2:");
+
+            // Initialization
+            score = 0;
+            health = 100;
+
+            ShowHUD();// show HUD
+            TakeDamage(5); // take damage
+            ShowHUD();
+            TakeDamage(10); // take damage
+            ShowHUD();
+            TakeDamage(50);
+            ShowHUD();
+
+            Console.ReadKey(true); // wait for keypress to exit
+
         }
     }
 }
